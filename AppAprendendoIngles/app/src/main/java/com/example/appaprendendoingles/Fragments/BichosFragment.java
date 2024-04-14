94bcb147f8555348dd85074c2e18cefee4af34ed
Gabrie Tangerina <gabriel.tangerina@hotmail.com>
2024-04-14 00:16:58 -0300

    Terminando app "Aprender Ingles"

--- AppAprendendoIngles/app/src/main/java/com/example/appaprendendoingles/Fragments/BichosFragment.java
+++ AppAprendendoIngles/app/src/main/java/com/example/appaprendendoingles/Fragments/BichosFragment.java
@@ -1,5 +1,6 @@
 package com.example.appaprendendoingles.Fragments;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -15,6 +16,7 @@ import com.example.appaprendendoingles.R;
 public class BichosFragment extends Fragment implements View.OnClickListener {
 
     private ImageView imageCao, imageGato, imageLeao, imageMacaco, imageOvelha, imageVaca;
+    private MediaPlayer mediaPlayer;
 
     public BichosFragment() {
         // Required empty public constructor
@@ -23,7 +25,7 @@ public class BichosFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View view = inflater.inflate(R.layout.fragment_bichos, container, false);
 
         imageCao = view.findViewById(R.id.imageCao);
@@ -33,6 +35,14 @@ public class BichosFragment extends Fragment implements View.OnClickListener {
         imageOvelha = view.findViewById(R.id.imageOvelha);
         imageVaca = view.findViewById(R.id.imageVaca);
 
+        // Aplicando evento de click. Usa-se o "this" para falar que o evento de click será tratado pela propria classe
+        imageCao.setOnClickListener(this);
+        imageGato.setOnClickListener(this);
+        imageLeao.setOnClickListener(this);
+        imageMacaco.setOnClickListener(this);
+        imageOvelha.setOnClickListener(this);
+        imageVaca.setOnClickListener(this);
+
 
 
         return view;
@@ -40,6 +50,50 @@ public class BichosFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        Toast.makeText()
+
+        if(v.getId() == R.id.imageCao){
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.dog);
+            tocarSom();
+        }else if(v.getId() == R.id.imageGato){
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.cat);
+            tocarSom();
+        }else if(v.getId() == R.id.imageLeao){
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.lion);
+            tocarSom();
+        }else if(v.getId() == R.id.imageMacaco){
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.monkey);
+            tocarSom();
+        }else if(v.getId() == R.id.imageOvelha){
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.sheep);
+            tocarSom();
+        }else if(v.getId() == R.id.imageVaca){
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.cow);
+            tocarSom();
+        }
+
     }
+
+    public void tocarSom(){
+        if(mediaPlayer != null){
+            mediaPlayer.start();
+
+            // Sempre que completar o som será liberado os recursos da memória armazenados ao usar o som
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mediaPlayer.release();
+                }
+            });
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
 }
--- AppAprendendoIngles/app/src/main/java/com/example/appaprendendoingles/Fragments/NumerosFragment.java
+++ AppAprendendoIngles/app/src/main/java/com/example/appaprendendoingles/Fragments/NumerosFragment.java
@@ -1,5 +1,6 @@
 package com.example.appaprendendoingles.Fragments;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.appaprendendoingles.R;
 
-public class NumerosFragment extends Fragment {
+public class NumerosFragment extends Fragment implements View.OnClickListener {
+
+    private ImageView imageNum1, imageNum2, imageNum3, imageNum4, imageNum5, imageNum6;
+    private MediaPlayer mediaPlayer;
 
     public NumerosFragment() {
         // Required empty public constructor
@@ -19,7 +24,69 @@ public class NumerosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_numeros, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_numeros, container, false);
+
+        imageNum1 = view.findViewById(R.id.imageNum1);
+        imageNum2 = view.findViewById(R.id.imageNum2);
+        imageNum3 = view.findViewById(R.id.imageNum3);
+        imageNum4 = view.findViewById(R.id.imageNum4);
+        imageNum5 = view.findViewById(R.id.imageNum5);
+        imageNum6 = view.findViewById(R.id.imageNum6);
+
+        imageNum1.setOnClickListener(this);
+        imageNum2.setOnClickListener(this);
+        imageNum3.setOnClickListener(this);
+        imageNum4.setOnClickListener(this);
+        imageNum5.setOnClickListener(this);
+        imageNum6.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.imageNum1){
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.one);
+            tocarSom();
+        } else if (v.getId() == R.id.imageNum2) {
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.two);
+            tocarSom();
+        }else if (v.getId() == R.id.imageNum3) {
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.three);
+            tocarSom();
+        }else if (v.getId() == R.id.imageNum4) {
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.four);
+            tocarSom();
+        }else if (v.getId() == R.id.imageNum5) {
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.five);
+            tocarSom();
+        }else if (v.getId() == R.id.imageNum6) {
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.six);
+            tocarSom();
+        }
+    }
+
+    public void tocarSom(){
+        if(mediaPlayer != null){
+            mediaPlayer.start();
+
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mediaPlayer.release();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
